104fb58d5caec166c3911442d92e7fdf97c1e539
ADDB <addb1234567890@gmail.com>
2023-11-09 00:23:21 +0100

    Finally works

--- content/RogueTrader.ModTemplate/Main.cs
+++ content/RogueTrader.ModTemplate/Main.cs
@@ -1,34 +1,27 @@
-﻿using UnityEngine;
+﻿using HarmonyLib;
+using System.Reflection;
 using UnityModManagerNet;
-using HarmonyLib;
 
 namespace RogueTrader.ModTemplate {
-#if DEBUG
     [EnableReloading]
-#endif
-	static class Main {
-        	public static bool Enabled;
-        	internal static Harmony HarmonyInstance;
+    static class Main {
+        public static bool Enabled;
+        internal static Harmony HarmonyInstance;
 
-		static bool Load(UnityModManager.ModEntry modEntry) {
-#if DEBUG
-			modEntry.OnUnload = OnUnload;
-#endif
-			modEntry.OnToggle = OnToggle;
-			modEntry.OnGUI = OnGUI;
-			HarmonyInstance = new Harmony(modEntry.Info.Id);
-			HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
-            		return true;
-        	}
+        static bool Load(UnityModManager.ModEntry modEntry) {
+            modEntry.OnUnload = OnUnload;
+            modEntry.OnGUI = OnGUI;
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
 
-		static void OnGUI() {
+        static void OnGUI(UnityModManager.ModEntry modEntry) {
 
-		}
-#if DEBUG
-        	static bool OnUnload(UnityModManager.ModEntry modEntry) {
-            		HarmonyInstance.UnpatchAll(modEntry.Info.Id);
-            		return true;
-        	}
-#endif
-    	}
+        }
+        static bool OnUnload(UnityModManager.ModEntry modEntry) {
+            HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+    }
 }
