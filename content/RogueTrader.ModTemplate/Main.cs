475512371e38b9e27ac861c58fdb6a3db59b245f
ADDB <addb1234567890@gmail.com>
2023-11-09 00:47:45 +0100

    W

--- content/RogueTrader.ModTemplate/Main.cs
+++ content/RogueTrader.ModTemplate/Main.cs
@@ -3,16 +3,12 @@ using System.Reflection;
 using UnityModManagerNet;
 
 namespace RogueTrader.ModTemplate {
-#if DEBUG
     [EnableReloading]
-#endif
     static class Main {
         internal static Harmony HarmonyInstance;
 
         static bool Load(UnityModManager.ModEntry modEntry) {
-#if DEBUG
             modEntry.OnUnload = OnUnload;
-#endif
             modEntry.OnGUI = OnGUI;
             HarmonyInstance = new Harmony(modEntry.Info.Id);
             HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
@@ -23,11 +19,9 @@ namespace RogueTrader.ModTemplate {
 
         }
 
-#if DEBUG
         static bool OnUnload(UnityModManager.ModEntry modEntry) {
             HarmonyInstance.UnpatchAll(modEntry.Info.Id);
             return true;
         }
-#endif
     }
 }
