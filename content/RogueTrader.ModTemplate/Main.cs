7d271bc062161755856c7eb83cb7cacf02d964ca
ADDB <addb1234567890@gmail.com>
2023-11-08 16:17:20 +0100

    Wow initial commit

---
+++ content/RogueTrader.ModTemplate/Main.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+using HarmonyLib;
+
+namespace RogueTrader.ModTemplate {
+#if DEBUG
+    [EnableReloading]
+#endif
+	static class Main {
+        	public static bool Enabled;
+        	internal static Harmony HarmonyInstance;
+
+		static bool Load(UnityModManager.ModEntry modEntry) {
+#if DEBUG
+			modEntry.OnUnload = OnUnload;
+#endif
+			modEntry.OnToggle = OnToggle;
+			modEntry.OnGUI = OnGUI;
+			HarmonyInstance = new Harmony(modEntry.Info.Id);
+			HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            		return true;
+        	}
+
+		static void OnGUI() {
+
+		}
+#if DEBUG
+        	static bool OnUnload(UnityModManager.ModEntry modEntry) {
+            		HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+            		return true;
+        	}
+#endif
+    	}
+}
