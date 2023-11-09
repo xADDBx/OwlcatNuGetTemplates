33a2397684a979978b18cd280a4fdc7740523141
ADDB <addb1234567890@gmail.com>
2023-11-09 01:55:40 +0100

    Rebrand to Owlcat.Templates; Add new template for Wrath

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
+## Existing Template:
+
+- `rtmod`  - UnityModManager Template for Warhammer 40,000: Rogue Trader
+- `wrathmod`  - UnityModManager Template for Pathfinder: Wrath of the Righteous
+
 ## Usage
 
 - Open command prompt in directory which should contain project directory
-- `dotnet new --install RogueTrader.Templates`
+- `dotnet new --install Owlcat.Templates`
 - `dotnet new rtmod -n ModID -D "Mod Name"`
 - **Build resulting project twice (there will be errors when first building, this is normal**
 - Restart your IDE to rebuild cache if there are still red underlines
---
+++ content/Wrath.ModTemplate/Main.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace RogueTrader.ModTemplate {
+    [EnableReloading]
+    static class Main {
+        internal static Harmony HarmonyInstance;
+
+        static bool Load(UnityModManager.ModEntry modEntry) {
+            modEntry.OnUnload = OnUnload;
+            modEntry.OnGUI = OnGUI;
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        static void OnGUI(UnityModManager.ModEntry modEntry) {
+
+        }
+
+        static bool OnUnload(UnityModManager.ModEntry modEntry) {
+            HarmonyInstance.UnpatchAll(modEntry.Info.Id);
+            return true;
+        }
+    }
+}
