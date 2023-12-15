fa6820aaf870e646db1d6fb51c21d2dd84a80c48
alterasc <alterasc@protonmail.com>
2023-12-15 17:53:34 +0100

    add solution to workshopmod template
    wrap reloading in preprocessor debug directives wrapped in cnd:noEmit
    
    move all files that are not packaged with mod away from project folder and into solution
    
    add Workshop-description.txt file to keep proper mod description there

--- content/RogueTrader.ModTemplate/Main.cs
+++ content/RogueTrader.ModTemplate/Main.cs
@@ -3,14 +3,22 @@ using System.Reflection;
 using UnityModManagerNet;
 
 namespace RogueTrader.ModTemplate {
+//-:cnd:noEmit
+#if DEBUG
     [EnableReloading]
+#endif
+//+:cnd:noEmit
     static class Main {
         internal static Harmony HarmonyInstance;
         internal static UnityModManager.ModEntry.ModLogger log;
 
         static bool Load(UnityModManager.ModEntry modEntry) {
             log = modEntry.Logger;
+//-:cnd:noEmit
+#if DEBUG
             modEntry.OnUnload = OnUnload;
+#endif
+//+:cnd:noEmit
             modEntry.OnGUI = OnGUI;
             HarmonyInstance = new Harmony(modEntry.Info.Id);
             HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
