37c1191c61c89ea20ea92780f095a07ce87335a6
ADDB <addb1234567890@gmail.com>
2024-04-23 04:52:30 +0200

    Add some common usings

--- content/Wrath.ModTemplate/Wrath.ModTemplate/Main.cs
+++ content/Wrath.ModTemplate/Wrath.ModTemplate/Main.cs
@@ -1,4 +1,9 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Reflection;
 using UnityModManagerNet;
 
