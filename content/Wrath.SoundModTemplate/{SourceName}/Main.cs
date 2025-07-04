87d97303e71d46d989ec35a03f8aa0cd69d6278f
xADDBx <62178123+xADDBx@users.noreply.github.com>
2025-07-04 03:38:38 +0200

    Wrath Sound

--- content/Wrath.SoundModTemplate/{SourceName}/Main.cs
+++ content/Wrath.SoundModTemplate/{SourceName}/Main.cs
@@ -1,38 +1,28 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 using UnityModManagerNet;
 using Kingmaker.Sound;
-using System.IO;
 using Kingmaker.Blueprints.JsonSystem;
 using System.Xml;
 using Kingmaker.Localization;
 
 namespace {SourceName};
 
-//-:cnd:noEmit
-#if DEBUG
-[EnableReloading]
-#endif
-//+:cnd:noEmit
 public static class Main {
     internal static Harmony HarmonyInstance;
-    internal static UnityModManager.ModEntry.ModLogger log;
+    internal static UnityModManager.ModEntry.ModLogger Log;
 
     public static bool Load(UnityModManager.ModEntry modEntry) {
-        log = modEntry.Logger;
-//-:cnd:noEmit
-#if DEBUG
-        modEntry.OnUnload = OnUnload;
-#endif
-//+:cnd:noEmit
+        Log = modEntry.Logger;
         modEntry.OnGUI = OnGUI;
         HarmonyInstance = new Harmony(modEntry.Info.Id);
-        HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        try {
+            HarmonyInstance = new Harmony(modEntry.Info.Id);
+        } catch {
+            HarmonyInstance.UnpatchAll(HarmonyInstance.Id);
+            throw;
+        }
         return true;
     }
 
@@ -50,7 +40,7 @@ public static class Main {
             var banksPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             try {
-                log.Log($"Add soundbank base path {banksPath}");
+                Log.Log($"Add soundbank base path {banksPath}");
                 AkSoundEngine.AddBasePath(banksPath);
 
                 foreach (var f in Directory.EnumerateFiles(banksPath, "*.bnk")) {
@@ -59,18 +49,18 @@ public static class Main {
                     if (bankName == "Init.bnk")
                         throw new InvalidOperationException("Do not include Init.bnk");
 
-                    log.Log($"Load soundbank {f}");
+                    Log.Log($"Load soundbank {f}");
 
                     var akResult = AkSoundEngine.LoadBank(bankName, out var bankId);
 
                     if (akResult == AKRESULT.AK_Success) {
                         LoadedBankIds.Add(bankId);
                     } else {
-                        log.Error($"Loading soundbank {f} failed with result {akResult}");
+                        Log.Error($"Loading soundbank {f} failed with result {akResult}");
                     }
                 }
             } catch (Exception e) {
-                log.LogException(e);
+                Log.LogException(e);
                 UnloadSoundbanks();
             }
         }
@@ -81,7 +71,7 @@ public static class Main {
                     AkSoundEngine.UnloadBank(bankId, IntPtr.Zero);
                     LoadedBankIds.Remove(bankId);
                 } catch (Exception e) {
-                    log.LogException(e);
+                    Log.LogException(e);
                 }
             }
         }
@@ -98,28 +88,20 @@ public static class Main {
 
                     if (bankName == "Init") continue;
 
-                    log.Log($"Adding dialog events from bank {bankName}");
+                    Log.Log($"Adding dialog events from bank {bankName}");
 
                     foreach (XmlElement e in n["IncludedEvents"]) {
                         var eventId = e.Attributes["Id"].Value;
                         var eventName = e.Attributes["Name"].Value;
 
-                        log.Log($"Add {eventName} -> {eventId}");
+                        Log.Log($"Add {eventName} -> {eventId}");
 
                         LocalizationManager.SoundPack.PutString(eventName, eventName);
                     }
                 }
             } catch (Exception e) {
-                log.LogException(e);
+                Log.LogException(e);
             }
         }
     }
-//-:cnd:noEmit
-#if DEBUG
-    public static bool OnUnload(UnityModManager.ModEntry modEntry) {
-        HarmonyInstance.UnpatchAll(modEntry.Info.Id);
-        return true;
-    }
-#endif
-//+:cnd:noEmit
 }
